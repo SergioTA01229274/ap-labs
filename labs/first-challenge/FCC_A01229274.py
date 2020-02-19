548f7e8169e8b3d89b9c3415df81890b09be5af3
Sergio Ivan Tostado Nieto <A01229274@itesm.mx>
2020-02-18 21:01:02 -0600

    First Challenge solved by Sergio Tostado
    Signed-off-by: Sergio Ivan Tostado Nieto <A01229274@itesm.mx>

---
+++ labs/first-challenge/FCC_A01229274.py
@@ -0,0 +1,21 @@
+import sys
+
+def max_substring(word):
+    temp = ""
+    max = [0, ""]
+    for i in range(0, len(word)):
+        if word[i] not in temp:
+            temp += word[i]
+        else:
+            if len(temp) > max[0]:
+                max[0] = len(temp)
+                max[1] = temp
+                temp = word[i]
+    if len(temp) > max[0]:
+        max[0] = len(temp)
+        max[1] = temp
+    return max[0]
+
+
+result = max_substring(sys.argv[1])
+print(f"Max substring length: {result}")
