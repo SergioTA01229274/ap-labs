cd159f8fdadea43d8f2e5e0ba1b1574d866eebbe
Sergio Ivan Tostado Nieto <A01229274@itesm.mx>
2020-03-01 13:06:43 -0600

    Month-day solved by Sergio Tostado
    Signed-off-by: Sergio Ivan Tostado Nieto <A01229274@itesm.mx>

--- labs/month-day/month_day.c
+++ labs/month-day/month_day.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+//Author: Sergio Ivan Tostado Nieto A01229274
 
-/* month_day function's prototype*/
-void month_day(int year, int yearday, int *pmonth, int *pday);
+void month_day(int year, int yearday, int* pyear, int* pday){
+	int months_values[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	char *months[12] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dic"};
+	if (year % 4 == 0)
+		months_values[1] = 29;
+	int month_day;
+	int tmp_yday = *pday;
+	int cont = 0;
+	for(int i = 0; i < 12; i++){
+		if (tmp_yday > months_values[i]){
+			tmp_yday = tmp_yday - months_values[i];
+			cont ++;
+		}else{
+			break;
+		}
+	}
+	printf("%s %d, %d\n", months[cont], tmp_yday, *pyear);
+}
 
-int main() {
-    return 0;
+int main(int argc, char* argv[]) {
+	int year = atoi(argv[1]);
+	int day = atoi(argv[2]);
+	int* p_year = &year;
+	int* p_day = &day;
+	month_day(year, day, p_year, p_day);
+	return 0;
 }
