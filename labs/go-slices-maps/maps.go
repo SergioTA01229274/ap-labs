fc39453150a2afc3d6a5d50437e487cc4af077f9
Sergio Ivan Tostado Nieto <A01229274@itesm.mx>
2020-03-10 19:04:38 -0600

    Go Slices and Maps solved by Sergio Tostado
    Signed-off-by: Sergio Ivan Tostado Nieto <A01229274@itesm.mx>

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,35 @@
+//Sergio Ivan Tostado Nieto
+//Student ID: A01229274
+
 package main
 
+import (
+	"fmt"
+	"strings"
+	"os"
+)
+
+func WordCount(word string) map[string]int {
+	res := make(map[string]int)
+	word_arr := strings.Split(word, " ")
+	for i := 0; i < len(word_arr); i++{
+		_, ok := res[word_arr[i]]
+		if(!ok){
+			res[word_arr[i]] = 1
+		}else{
+			res[word_arr[i]] += 1;
+		}
+	}
+	return res
+}
+
+
+
 func main() {
+	arg := os.Args[1]
+	for j := 2; j < len(os.Args); j++{
+		arg += " "
+		arg += os.Args[j]
+	}
+	fmt.Printf("%v\n",WordCount(arg))
 }
--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -1,4 +1,26 @@
+//Author: Sergio Ivan Tostado Nieto
+//Student ID: A01229274
+
+
 package main
 
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	slice := make([][]uint8, dy)
+	for i := 0; i < dy; i++ {
+		slice[i] = make([]uint8,dx)
+	}
+	var f uint8 = 5
+	for j := 0; j < dy; j++{
+		for k := 0; k < len(slice[j]); k++{
+			slice[j][k] = f * 2
+		}
+	f += 4
+	}
+	return slice
+}
+
 func main() {
+	pic.Show(Pic)
 }
