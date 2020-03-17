aafdf4f2b535909a837692b4facf3d739436f976
Sergio Ivan Tostado Nieto <A01229274@itesm.mx>
2020-03-17 13:40:09 -0600

    First Partial challenge solved by Sergio Tostado, Directory scanner in Golang and Pacman log analyzer in C
    Signed-off-by: Sergio Ivan Tostado Nieto <A01229274@itesm.mx>

--- challenges/first-partial/dir-scan.go
+++ challenges/first-partial/dir-scan.go
@@ -1,21 +1,107 @@
+//Author: Sergio Ivan Tostado Nieto
+//Student ID: A01229274
+//Delivery: First partial challenge, Directory scannner in Golang
+
 package main
 
 import (
 	"fmt"
 	"os"
+	"log"
+	"io/ioutil"
 )
+var tmp_dirs[]string
 
-// scanDir stands for the directory scanning implementation
-func scanDir(dir string) error {
-	return nil
+func scanDir(dir string) map[string] int{
+
+	files, err := ioutil.ReadDir(dir)
+	categories := make(map[string]int)
+	categories["Directories"] = 0
+	categories["Symbolic Links"] = 0
+        categories["Devices"] = 0
+        categories["Sockets"] = 0
+        categories["Reg Files"] = 0
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tmp_dir_flag := false
+	for j := 0; j < len(files); j++ {
+		switch mode := files[j].Mode(); {
+			case mode&os.ModeDir != 0:
+				tmp_wd, _ := os.Getwd()
+				if !tmp_dir_flag {
+					tmp_dir_flag = true
+					tmp_dirs = make([]string, 1)
+					tmp_dirs[0] = tmp_wd + "/" + files[j].Name()
+					categories["Directories"] += 1
+					continue
+				}
+				tmp_dirs = append(tmp_dirs, tmp_wd + "/" + files[j].Name())
+				categories["Directories"] += 1
+			case mode&os.ModeSymlink != 0:
+				categories["Symbolic Links"] += 1
+			case mode&os.ModeDevice != 0:
+				categories["Devices"] += 1
+			case mode&os.ModeSocket != 0:
+				categories["Sockets"] += 1
+			default:
+				categories["Reg Files"] += 1
+		}
+	}
+
+	for len(tmp_dirs) > 0 {
+		dir_2_scan := tmp_dirs[0]
+		count_files(dir_2_scan, categories)
+		tmp_dirs = tmp_dirs[1:]
+	}
+	return categories
 }
 
-func main() {
+func count_files(tmp_dir string, categos map[string]int) {
+	os.Chdir(tmp_dir)
+	tmp_files, tmp_err := ioutil.ReadDir(tmp_dir)
+	if tmp_err != nil {
+                log.Fatal(tmp_err)
+        }
 
+	for j := 0; j < len(tmp_files); j++ {
+                switch mode := tmp_files[j].Mode(); {
+                       	case mode&os.ModeDir != 0:
+                               	tmp_wd, _ := os.Getwd()
+                               	tmp_dirs = append(tmp_dirs, tmp_wd + "/" + tmp_files[j].Name())
+                               	categos["Directories"] += 1
+                       	case mode&os.ModeSymlink != 0:
+                               	categos["Symbolic Links"] += 1
+                       	case mode&os.ModeDevice != 0:
+                               	categos["Devices"] += 1
+                       	case mode&os.ModeSocket != 0:
+                               	categos["Sockets"] += 1
+                       	default:
+                               	categos["Reg Files"] += 1
+               	}
+       	}
+}
+
+
+func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("Usage: ./dir-scan <directory>")
 		os.Exit(1)
 	}
+	os.Chdir(os.Args[1])
+	scan_res := scanDir(os.Args[1])
+	fmt.Println("Directory scanner tool")
+	fmt.Println("")
+	fmt.Println("+-------------------------+--------------------+")
+        fmt.Printf( "|Path                     |%s\n", os.Args[1])
+        fmt.Println("+-------------------------+--------------------+")
+        fmt.Printf( "|Directories              |         %v         |\n", scan_res["Directories"])
+        fmt.Printf( "|Symbolic Links           |         %v         |\n", scan_res["Symbolic Links"])
+        fmt.Printf( "|Devices                  |         %v         |\n", scan_res["Devices"])
+        fmt.Printf( "|Sockets                  |         %v         |\n", scan_res["Sockets"])
+        fmt.Printf( "|Other files              |         %v         |\n", scan_res["Reg Files"])
+        fmt.Println("+-------------------------+--------------------+")
 
-	scanDir(os.Args[1])
 }
--- challenges/first-partial/pacman-analyzer.c
+++ challenges/first-partial/pacman-analyzer.c
@@ -1,4 +1,13 @@
+//Author: Sergio Ivan Tostado Nieto
+//Student ID: A01229274
+//Delivery: Firs partial challenge, Pacman Log analyzer in C
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+
 
 #define REPORT_FILE "packages_report.txt"
 
@@ -16,10 +25,229 @@ int main(int argc, char **argv) {
     return 0;
 }
 
-void analizeLog(char *logFile, char *report) {
-    printf("Generating Report from: [%s] log file\n", logFile);
+char ** getPackageInfo(char *line){
+	int cont = 0;
+	int flag = 1;
+	int c_char = 0;
+	int str_len = 0;
+	while(flag){
+		if (*(line + cont) == ']'){
+			c_char++;
+		}
+		if (c_char == 2){
+			switch (*(line + cont + 2)){
+				case 'i':
+					str_len = 10; // installed package
+					break;
+				case 'u':
+					str_len = 9; // upgraded package
+					break;
+				case 'r':
+					if (*(line + cont + 3) == 'u' || *(line + cont + 4) == 'i'){
+						return NULL;
+					}
+					str_len = 8; // removed package
+					break;
+				default:
+					return NULL; // useless line
+			}
+			flag = 0;
+		}
+		cont++;
+	}
+	char *packageDate = malloc(sizeof(char) * 17);
+	char *key_word = malloc(sizeof(char) * str_len);
+	char *packageName = malloc(sizeof(char) * 75);
+	for(cont = 0; cont < 16; cont++){
+		packageDate[cont] = *(line + cont + 1);
+	}
+	packageDate[16] = '\0';
 
-    // Implement your solution here.
+	flag = 1;
+	c_char = 0;
+	while(flag){
+		if (*(line + cont) == ']'){
+			c_char++;
+		}
+		cont++;
+		if (c_char == 2){
+                	break;
+                }
+	}
 
-    printf("Report is generated at: [%s]\n", report);
+	int j = 0;
+	while(j < str_len){
+		if (*(line + cont + 1) == ' '){
+			break;
+		}
+		key_word[j] = *(line + cont + 1);
+		cont++;
+		j++;
+	}
+	key_word[j + 1] = '\0';
+
+	cont++;
+	j = 0;
+	while(j < 75){
+                if (*(line + cont + 1) == ' '){
+                        break;
+                }
+                packageName[j] = *(line + cont + 1);
+                cont++;
+                j++;
+
+	}
+	packageName[j + 1] = '\0';
+
+	static char *res[3];
+	res[0] = packageDate;
+	res[1] = packageName;
+	res[2] = key_word;
+	return res;
 }
+
+
+
+char * getLine(char *lines, int lineStart){
+	int cont = lineStart;
+	static char res[500];
+	int arr_cont = 0;
+	while ( *(lines + cont) != '\n'){
+	      	res[arr_cont] = *(lines + cont);
+		arr_cont++;
+		cont++;
+        }
+	res[arr_cont] = '\0';
+	return res;
+}
+
+struct packageInfo {
+        char *Name;
+        char *install_d;
+        char *last_u;
+        int updates;
+        char *removal_d;
+};
+
+void analizeLog(char *logFile, char *report) {
+	printf("Generating Report from: [%s] log file\n\n", logFile);
+
+	int file_d = open(logFile, O_RDONLY);
+	if (file_d == -1){
+		printf("Error Number %d\n", errno);
+		perror("Failure when trying to open log file");
+		exit(1);
+	}
+
+        int ls = lseek(file_d, 10, SEEK_END);
+	char *content = (char *) calloc(1000, ls * sizeof(char));
+	if (close(file_d) < 0){
+		perror("Failure when trying to close log file");
+		exit(1);
+	}
+
+	int sz;
+	file_d = open(logFile, O_RDONLY);
+        sz = read(file_d, content, ls);
+
+	int c = 0;
+	int y = 0;
+	struct packageInfo packages[1000];
+	int t = 0;
+
+	int t_installed = 0;
+	int t_upgraded = 0;
+	int t_removed = 0;
+	while (y < 3862){
+		char *line;
+		if (c == 0){
+			line = getLine(content, c);
+		}else{
+			//char *line_tmp = getLine(content, c + 1);
+			line = getLine(content, c + 1);
+		}
+		int u = 0;
+		while(*(line + u) != '\0'){
+			u++;
+                }
+		if (c != 0){
+			if (*line != '['){
+				c += u + 1;
+				y++;
+				continue;
+			}
+		}
+		c += u + 1;
+		char **line_results = getPackageInfo(line);
+		if(line_results == NULL){
+			y++;
+			continue;
+		}
+
+		if(strcmp(line_results[2], "installed") == 0){
+			struct packageInfo tmp_package = {line_results[1], line_results[0], line_results[0], 0, "-"};
+			packages[t] = tmp_package;
+			t_installed++;
+			t++;
+		}else if(strcmp(line_results[2], "upgraded") == 0){
+			int tmp_cont = 0;
+			while(strcmp(line_results[1], packages[tmp_cont].Name) != 0){
+				tmp_cont++;
+			}
+			packages[tmp_cont].last_u = line_results[0];
+			packages[tmp_cont].updates += 1;
+                        t_upgraded++;
+                }else if(strcmp(line_results[2], "removed") == 0){
+                        int tmp_cont = 0;
+                        while(strcmp(line_results[1], packages[tmp_cont].Name) != 0){
+                                tmp_cont++;
+                        }
+                        packages[tmp_cont].removal_d = line_results[0];
+                        t_removed++;
+                }
+		y++;
+	}
+	printf("Pacman Packages general report%s", "\n\n");
+	printf("----------------------%s", "\n");
+        printf("- Installed packages : %d\n", t_installed);
+        printf("- Removed  packages  : %d\n", t_removed);
+        printf("- Upgraded packages  : %d\n", t_upgraded);
+        printf("- Current installed  : %d\n\n", t_installed - t_removed);
+
+	close(file_d);
+	free(content);
+
+	printf("Report is generated at: [%s]\n", report);
+
+	packages[t + 1].Name  = NULL;
+	FILE *fptr;
+	fptr = fopen(REPORT_FILE, "w+");
+	t = 0;
+	while(packages[t].Name != NULL){
+		fputs("- Package Name\t : " ,fptr);
+		fputs(packages[t].Name, fptr);
+		fputs("\n", fptr);
+		fputs("  - Install date\t : ", fptr);
+		fputs(packages[t].install_d, fptr);
+                fputs("\n", fptr);
+                fputs("  - Last update date\t : ", fptr);
+                fputs(packages[t].last_u, fptr);
+                fputs("\n", fptr);
+
+		char tmp_up[10];
+                fputs("  - How many updates\t : ", fptr);
+                sprintf(tmp_up, "%d", packages[t].updates);
+		fputs(tmp_up, fptr);
+                fputs("\n", fptr);
+
+                fputs("  - Removal date\t : ", fptr);
+                fputs(packages[t].removal_d, fptr);
+                fputs("\n\n", fptr);
+		t++;
+
+	}
+
+
+	fclose(fptr);
+}
+
