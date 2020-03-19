40d0f4ce5c1f4c68f3d1c877efe6f54f1060053c
Sergio Ivan Tostado Nieto <A01229274@itesm.mx>
2020-03-18 21:59:26 -0600

    String manipulation lab solved by Sergio Tostado
    Signed-off-by: Sergio Ivan Tostado Nieto <A01229274@itesm.mx>

--- labs/str-lib/main.c
+++ labs/str-lib/main.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
-int main() {
-    return 0;
+
+int mystrlen(char *str);
+char *mystradd(char *origin, char *addition);
+int mystrfind(char *origin, char *substr);
+
+
+int main(int argc, char **argv) {
+	if(argc < 4){
+		printf("Usage: ./main2 [original str] [str to add] [substring to find]%s", "\n");
+		return 1;
+	}
+	char *str_add = mystradd(argv[1], argv[2]);
+	printf("Initial Length: %d\n", mystrlen(argv[1]));
+	printf("New String: %s\n", str_add);
+	printf("%d\n", mystrfind(str_add, argv[3]));
+	return 0;
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,60 @@
-
+#include <stdlib.h>
 
 int mystrlen(char *str){
-    return 0;
+	int i = 0;
+	if(str == NULL){
+		return -1;
+	}
+	while(*(str + i) != '\0'){
+		i++;
+	}
+	return i;
 }
 
-char *mystradd(char *origin, char *addition){
-    return 0;
+char * mystradd(char *origin, char *addition){
+	if(origin == NULL || addition == NULL){
+		return '\0';
+	}
+	int i = 0;
+	int j = 0;
+	while(*(origin + i) != '\0'){
+		i++;
+	}
+	while(*(addition + j) != '\0'){
+                j++;
+        }
+	char *res = (char *)malloc(sizeof(char) * (i + j + 1));
+	int k = 0;
+	while(k < i){
+		res[k] = *(origin + k);
+		k++;
+	}
+	k = 0;
+        while(k < j){
+                res[k + i] = *(addition + k);
+                k++;
+        }
+	res[i + j] = '\0';
+	return res;
 }
 
 int mystrfind(char *origin, char *substr){
-    return 0;
+	int or_ptr = 0;
+	int sub_ptr = 0;
+	int flag = 1;
+	while(*(origin + or_ptr) != '\0'){
+		if(*(origin + or_ptr) == *(substr)){
+			flag = 1;
+			while(*(substr + sub_ptr) != '\0' && *(origin + or_ptr) != '\0'){
+				if(*(substr + sub_ptr) != *(origin + or_ptr)){
+					flag = 0;
+					break;
+				}
+				sub_ptr++;
+				or_ptr++;
+			}
+		}
+		or_ptr++;
+	}
+	return (flag)? 1:0;
 }
