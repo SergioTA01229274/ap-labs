22561a3fe317e1b58b936a07663f9dfb8edf47d6
Sergio Tostado <a01229274@itesm.mx>
2020-04-27 21:10:45 -0500

    Lab clockwall solved by Sergio Tostado
    Signed-off-by: Sergio Tostado <a01229274@itesm.mx>

--- labs/clockwall/clock2.go
+++ labs/clockwall/clock2.go
@@ -1,35 +1,59 @@
-// Clock2 is a concurrent TCP server that periodically writes the time.
 package main
 
 import (
-	"io"
-	"log"
-	"net"
-	"time"
+        "io"
+        "log"
+        "net"
+        "time"
+        "os"
+        "fmt"
+	"errors"
 )
 
-func handleConn(c net.Conn) {
-	defer c.Close()
-	for {
-		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+func handleConn(c net.Conn, tz string) {
+        defer c.Close()
+        for {
+
+                _, tErr := time.LoadLocation(tz)
+                if tErr != nil {
+			fmt.Println("Error when trying to load the timezone: " + tz)
+                        log.Print(tErr)
+			break
+                }
+                _, err := io.WriteString(c, tz + " " + time.Now().Format("15:04:05\n"))
+
 		if err != nil {
-			return // e.g., client disconnected
-		}
-		time.Sleep(1 * time.Second)
-	}
+                        log.Print(err)
+			break
+                }
+                time.Sleep(1 * time.Second)
+		errors.New("EOF") //doubt
+        }
 }
 
 func main() {
-	listener, err := net.Listen("tcp", "localhost:9090")
-	if err != nil {
-		log.Fatal(err)
-	}
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			log.Print(err) // e.g., connection aborted
-			continue
-		}
-		go handleConn(conn) // handle connections concurrently
-	}
+        args := os.Args[1:]
+        if len(args) < 2 {
+                fmt.Println("Usage: go run clock2.go -port [port number to use]")
+                os.Exit(1)
+        }
+
+        port := args[1]
+        ListenInstruction := "localhost:" + port
+
+        listener, err := net.Listen("tcp", ListenInstruction)
+        if err != nil {
+                log.Fatal(err)
+        }
+
+        timezone := os.Getenv("TZ")
+        for {
+                conn, err := listener.Accept()
+                if err != nil {
+                        log.Print(err) // e.g., connection aborted
+                        continue
+                }
+                go handleConn(conn, timezone) // handle connections concurrently
+        }
 }
+
---
+++ labs/clockwall/clockWall.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+        "io"
+        "log"
+        "net"
+        "os"
+        "fmt"
+        "strings"
+	"sync"
+)
+
+
+func requestTime(dst io.Writer, src io.Reader, c chan bool){
+		if <- c{
+			_, err := io.Copy(dst, src)
+       			if err != nil {
+          			log.Fatal(err)
+       			}
+		}
+}
+
+
+func main(){
+        if len(os.Args) < 2{
+                fmt.Println("Usage: go run clockWall.go [first connection] [second connection] ...")
+                os.Exit(1)
+        }
+        Args := os.Args[1:]
+	connections := make([]net.Conn, len(Args))
+	channels := make([]chan bool, len(Args))
+	for j := 0; j < len(connections); j++{
+		conn, err := net.Dial("tcp", strings.Split(Args[j], "=")[1])
+                if err != nil{
+                        log.Fatal(err)
+                }
+                connections[j] = conn
+		channels[j] = make(chan bool, 1)
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(len(connections))
+	for i := 0; i < len(connections); i++{
+		go requestTime(os.Stdout, connections[i], channels[i])
+	}
+
+	for k := 0; k < len(channels); k++{
+		channels[k] <- true
+	}
+
+	wg.Wait()
+	defer func(chans [] chan bool){
+		for i := 0; i < len(channels); i++{
+			close(channels[i])
+		}
+	}(channels)
+
+	defer func(conns [] net.Conn){
+		for i := 0; i < len(conns); i++{
+			conns[i].Close()
+		}
+	}(connections)
+}
+
